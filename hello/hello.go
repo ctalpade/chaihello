f7181d90e2c729771916c40a0986a506d0a692de
chaitanya talpade <chaitanyais@yahoo.com>
2022-04-18 07:56:38 -0400

    2nd commit

--- hello/hello.go
+++ hello/hello.go
@@ -3,3 +3,8 @@ package hello
 func SayHello() string {
 	return "Hello, world from Chaitanya Talpade"
 }
+
+
+func OneMore(s string) string{
+	return "new func called with arg "+s
+}
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -1,10 +1,21 @@
 package hello
 
-import "testing"
+import ("fmt"
+		"testing"
+		)
 
 func TestHello(t *testing.T) {
 	want := "Hello, world from Chaitanya Talpade"
 	if got := SayHello(); got != want {
 		t.Errorf("Hello() = %q, want %q", got, want)
 	}
+}
+
+func TestOneMore(t *testing.T) {
+	want := "Hello, world from Chaitanya Talpade"
+	if got := OneMore("Chai"); got == want {
+		t.Errorf("Hello() = %q, want %q", got, want)
+	}
+	
+	fmt.Println("OneMore func called")
 }
